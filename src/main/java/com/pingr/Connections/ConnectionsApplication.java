4f48b3813c086409f257d20813a055a6c502dbb0
João Daniel <jotaf.daniel@gmail.com>
2022-02-07 16:34:03 -0300

    initial commit
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ src/main/java/com/pingr/Connections/ConnectionsApplication.java
@@ -0,0 +1,13 @@
+package com.pingr.Connections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pingr/Connections/ConnectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingr.Connections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
